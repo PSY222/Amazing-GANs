4d6abecf5e528869bc684d47342b09e3f2d01532
PSY222 <ss26348@naver.com>
2023-02-01 23:13:22 +0900

    DCGAN_with separate training real/fake mini batches

--- DCGAN/DCGAN_code.py
+++ DCGAN/DCGAN_code.py
@@ -1,30 +1,178 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "name": "python",
-   "version": "3.10.9 (tags/v3.10.9:1dd9be6, Dec  6 2022, 20:01:21) [MSC v.1934 64 bit (AMD64)]"
-  },
-  "orig_nbformat": 4,
-  "vscode": {
-   "interpreter": {
-    "hash": "5571f001800ec7d11ee8e69704548b3db90c132a9579c2f61d251600993f38b5"
-   }
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+import numpy as np
+import pandas as pd
+import cv2
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+import torchvision.utils as vutils
+import matplotlib.pyplot as plt
+import os
+import random
+from torchvision.io import read_image
+
+randomSeed = 22
+random.seed(randomSeed)
+torch.manual_seed(randomSeed)
+
+workers = 2
+batch_size  =64
+
+
+train = datasets.FashionMNIST(root="data",train=True,download=True,transform=ToTensor())
+test = datasets.FashionMNIST(root="data",train=False,download=True,transform=ToTensor())
+
+train_dataloader = DataLoader(train, batch_size = batch_size, shuffle=True,num_workers = workers)
+test_dataloader = DataLoader(test,batch_size=batch_size,shuffle = True,num_workers = workers)
+
+ngf = 64    #feature map size in generator
+ndf = 64    # feature map size in discriminatior
+nc = 1      #number of channel(FashionMNIST data is gray image)
+nz = 100    #size of z latent vector
+num_epochs = 5
+lr = 0.0002
+beta1 = 0.5
+ngpu = 1 #number of GPU
+device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data,0.0,0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data,1.0,0.02)
+        nn.init.constant_(m.bias.data,0)
+
+class Generator(nn.Module):
+    def __init__(self, ngpu):
+        super(Generator, self).__init__()
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+            nn.ConvTranspose2d( nz, ngf * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(ngf * 8),
+            nn.ReLU(True),
+
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 4),
+            nn.ReLU(True),
+
+            nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 2),
+            nn.ReLU(True),
+
+            nn.ConvTranspose2d(ngf * 2,ngf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf),
+            nn.ReLU(True),
+            nn.ConvTranspose2d( ngf, nc, kernel_size=1, stride=1, padding=2, bias=False),
+            nn.Tanh()
+        )
+
+    def forward(self, input):
+        return self.main(input)
+
+
+class Discriminator(nn.Module):
+    def __init__(self, ngpu):
+        super(Discriminator, self).__init__()
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+            nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+      
+            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+    
+            nn.Conv2d(ndf * 4, 1, 4, 2, 1, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, input):
+        return self.main(input)
+
+
+netG = Generator(ngpu).to(device)
+netG.apply(weights_init)   #initialize weights
+
+netD = Discriminator(ngpu).to(device)
+netD.apply(weights_init)
+
+criterion = nn.BCELoss()
+fixed_noise = torch.randn(64, nz, 1, 1, device=device)
+
+real_label = 1.
+fake_label = 0.
+optimizerD = optim.Adam(netD.parameters(), lr=lr, betas=(beta1, 0.999))
+optimizerG = optim.Adam(netG.parameters(), lr=lr, betas=(beta1, 0.999))
+
+img_list1 = []
+G_losses1 = []
+D_losses1 = []
+iters = 0
+
+# Different mini batches for real/fake to improve the performance
+for epoch in range(num_epochs):
+    for i , data in enumerate(train_dataloader,0):
+        #Update discriminator
+        ## Train with all-real batch
+        netD.zero_grad()
+        real_cpu = data[0].to(device) 
+        b_size = real_cpu.size(0)
+        label = torch.full((b_size,), real_label, dtype=torch.float, device=device)
+        output = netD(real_cpu).view(-1)
+        errD_real = criterion(output, label)
+        errD_real.backward()
+        D_x = output.mean().item()
+
+        ## Train with all-fake batch
+        noise = torch.randn(b_size, nz, 1, 1, device=device)
+        fake = netG(noise)
+        label.fill_(fake_label)
+        output = netD(fake.detach()).view(-1)
+        errD_fake = criterion(output, label)
+        errD_fake.backward()
+        D_G_z1 = output.mean().item()
+        
+        errD = errD_real + errD_fake
+        optimizerD.step()
+
+       #Update generator
+        netG.zero_grad()
+        label.fill_(real_label) 
+        output = netD(fake).view(-1)
+        errG = criterion(output, label)
+        errG.backward()
+        D_G_z2 = output.mean().item()
+        optimizerG.step()
+
+        # Output training stats
+        if i % 500 == 0:
+            print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
+                  % (epoch, num_epochs, i, len(train_dataloader),
+                     errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
+
+        # Save Losses for plotting later
+        G_losses1.append(errG.item())
+        D_losses1.append(errD.item())
+
+        # Check how the generator is doing by saving G's output on fixed_noise
+        if (iters % 500 == 0) or ((epoch == num_epochs-1) and (i == len(train_dataloader)-1)):
+            with torch.no_grad():
+                fake = netG(fixed_noise).detach().cpu()
+            img_list1.append(vutils.make_grid(fake, padding=2, normalize=True))
+        iters += 1
+
+
+#Visualization
+plt.figure(figsize=(10,5))
+plt.title("Generator and Discriminator Loss During Training")
+plt.plot(G_losses1,label="G")
+plt.plot(D_losses1,label="D")
+plt.xlabel("iterations")
+plt.ylabel("Loss")
+plt.legend()
+plt.show()
