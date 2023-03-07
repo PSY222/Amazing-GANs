4f286ef6f53319f3a175ccad795b5793f1e04ec7
PSY222 <ss26348@naver.com>
2023-03-07 11:31:54 +0900

    GANs evaluation folder created

---
+++ Evaluation_of_GANs/Frechet_Inception_Distance(FID).py
@@ -0,0 +1,140 @@
+import torch
+import numpy as np
+from torch import nn
+from tqdm.auto import tqdm
+from torchvision import transforms
+from torchvision.datasets import CelebA
+from torchvision.utils import make_grid
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+torch.manual_seed(0) 
+
+#Build generator
+class Generator(nn.Module):
+    def __init__(self, z_dim=10, im_chan=3, hidden_dim=64):
+        super(Generator, self).__init__()
+        self.z_dim = z_dim
+      
+        self.gen = nn.Sequential(
+            self.make_gen_block(z_dim, hidden_dim * 8),
+            self.make_gen_block(hidden_dim * 8, hidden_dim * 4),
+            self.make_gen_block(hidden_dim * 4, hidden_dim * 2),
+            self.make_gen_block(hidden_dim * 2, hidden_dim),
+            self.make_gen_block(hidden_dim, im_chan, kernel_size=4, final_layer=True),
+        )
+
+    def make_gen_block(self, input_channels, output_channels, kernel_size=3, stride=2, final_layer=False):
+        if not final_layer:
+            return nn.Sequential(
+                nn.ConvTranspose2d(input_channels, output_channels, kernel_size, stride),
+                nn.BatchNorm2d(output_channels),
+                nn.ReLU(inplace=True),
+            )
+        else:
+            return nn.Sequential(
+                nn.ConvTranspose2d(input_channels, output_channels, kernel_size, stride),
+                nn.Tanh(),
+            )
+
+    def forward(self, noise):
+        x = noise.view(len(noise), self.z_dim, 1, 1)
+        return self.gen(x)
+
+def get_noise(n_samples, z_dim, device='cpu'):
+    return torch.randn(n_samples, z_dim, device=device)
+
+#Load pre-trained model
+z_dim = 64
+image_size = 299
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+transform = transforms.Compose([
+    transforms.Resize(image_size),
+    transforms.CenterCrop(image_size),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+])
+
+in_coursera = False # Set this to false if you're running this outside Coursera
+if in_coursera:
+    import numpy as np
+    data = torch.Tensor(np.load('fid_images_tensor.npz', allow_pickle=True)['arr_0'])
+    dataset = torch.utils.data.TensorDataset(data, data)
+else:
+    dataset = CelebA(".", download=True, transform=transform)
+
+gen = Generator(z_dim).to(device)
+gen.load_state_dict(torch.load(f"pretrained_celeba.pth", map_location=torch.device(device))["gen"])
+gen = gen.eval()
+
+#Inception-v3 Network
+from torchvision.models import inception_v3
+inception_model = inception_v3(pretrained=False)
+inception_model.load_state_dict(torch.load("inception_v3_google-1a9a5a14.pth"))
+inception_model.to(device)
+inception_model = inception_model.eval() 
+
+#Frechet Inception Distance
+inception_model.fc = torch.nn.Identity()
+
+import scipy
+# This is the matrix square root function you will be using
+def matrix_sqrt(x):
+    y = x.cpu().detach().numpy()
+    y = scipy.linalg.sqrtm(y)
+    return torch.Tensor(y.real, device=x.device)
+
+
+def frechet_distance(mu_x, mu_y, sigma_x, sigma_y):
+    return  (mu_x - mu_y).dot(mu_x - mu_y) + torch.trace(sigma_x) + torch.trace(sigma_y) - 2*torch.trace(matrix_sqrt(sigma_x @ sigma_y)) 
+
+def preprocess(img):
+    img = torch.nn.functional.interpolate(img, size=(299, 299), mode='bilinear', align_corners=False)
+    return img
+
+import numpy as np
+def get_covariance(features):
+    return torch.Tensor(np.cov(features.detach().numpy(), rowvar=False))
+
+fake_features_list = []
+real_features_list = []
+
+gen.eval()
+n_samples = 512 # The total number of samples
+batch_size = 4 # Samples per iteration
+
+dataloader = DataLoader(
+    dataset,
+    batch_size=batch_size,
+    shuffle=True)
+
+#get the features of real and fake
+cur_samples = 0
+with torch.no_grad(): 
+    try:
+        for real_example, _ in tqdm(dataloader, total=n_samples // batch_size): # Go by batch
+            real_samples = real_example
+            real_features = inception_model(real_samples.to(device)).detach().to('cpu') # Move features to CPU
+            real_features_list.append(real_features)
+
+            fake_samples = get_noise(len(real_example), z_dim).to(device)
+            fake_samples = preprocess(gen(fake_samples))
+            fake_features = inception_model(fake_samples.to(device)).detach().to('cpu')
+            fake_features_list.append(fake_features)
+            cur_samples += len(real_samples)
+            if cur_samples >= n_samples:
+                break
+    except:
+        print("Error in loop")
+
+fake_features_all = torch.cat(fake_features_list)
+real_features_all = torch.cat(real_features_list)
+
+mu_fake = fake_features_all.mean(0)
+mu_real = real_features_all.mean(0)
+sigma_fake = get_covariance(fake_features_all)
+sigma_real = get_covariance(real_features_all)
+
+#Finally calculate FID
+with torch.no_grad():
+    print(frechet_distance(mu_real, mu_fake, sigma_real, sigma_fake).item())
---
+++ Evaluation_of_GANs/README.md
@@ -0,0 +1,4 @@
+## Evaluation of GANs Overview
+Frechet_Inception_Distance(FID)  <br>
+: (Source) Coursera GANs Specialization C2W1_Assignment with CelebA dataset <br>
+
